4d14160ed375619aa3adb5da335c84dfab40b9a4
Jefmans <42002519+Jefmans@users.noreply.github.com>
2025-04-22 14:09:53 +0200

    new unzip

--- .web/utils/context.js
+++ .web/utils/context.js
@@ -1,7 +1,7 @@
 import { createContext, useContext, useMemo, useReducer, useState } from "react"
 import { applyDelta, Event, hydrateClientStorage, useEventLoop, refs } from "$/utils/state.js"
 
-export const initialState = {"reflex___state____state": {"is_hydrated": false, "router": {"session": {"client_token": "", "client_ip": "", "session_id": ""}, "headers": {"host": "", "origin": "", "upgrade": "", "connection": "", "cookie": "", "pragma": "", "cache_control": "", "user_agent": "", "sec_websocket_version": "", "sec_websocket_key": "", "sec_websocket_extensions": "", "accept_encoding": "", "accept_language": "", "raw_headers": {}}, "page": {"host": "", "path": "", "raw_path": "", "full_path": "", "full_raw_path": "", "params": {}}}}, "reflex___state____state.reflex___state____update_vars_internal_state": {}, "reflex___state____state.reflex___state____on_load_internal_state": {}, "reflex___state____state.reflex___state____frontend_event_exception_state": {}, "reflex___state____state.frontend___pages___search____search_state": {"query": "", "result": ""}}
+export const initialState = {"reflex___state____state": {"is_hydrated": false, "router": {"session": {"client_token": "", "client_ip": "", "session_id": ""}, "headers": {"host": "", "origin": "", "upgrade": "", "connection": "", "cookie": "", "pragma": "", "cache_control": "", "user_agent": "", "sec_websocket_version": "", "sec_websocket_key": "", "sec_websocket_extensions": "", "accept_encoding": "", "accept_language": "", "raw_headers": {}}, "page": {"host": "", "path": "", "raw_path": "", "full_path": "", "full_raw_path": "", "params": {}}}}, "reflex___state____state.reflex___state____update_vars_internal_state": {}, "reflex___state____state.frontend___pages___search____search_state": {"query": "", "result": ""}, "reflex___state____state.reflex___state____on_load_internal_state": {}, "reflex___state____state.reflex___state____frontend_event_exception_state": {}}
 
 export const defaultColorMode = "system"
 export const ColorModeContext = createContext(null);
@@ -10,9 +10,9 @@ export const DispatchContext = createContext(null);
 export const StateContexts = {
   reflex___state____state: createContext(null),
   reflex___state____state__reflex___state____update_vars_internal_state: createContext(null),
+  reflex___state____state__frontend___pages___search____search_state: createContext(null),
   reflex___state____state__reflex___state____on_load_internal_state: createContext(null),
   reflex___state____state__reflex___state____frontend_event_exception_state: createContext(null),
-  reflex___state____state__frontend___pages___search____search_state: createContext(null),
 }
 export const EventLoopContext = createContext(null);
 export const clientStorage = {"cookies": {}, "local_storage": {}, "session_storage": {}}
@@ -52,7 +52,7 @@ export const initialEvents = () => [
 
 export const isDevMode = false
 
-export const lastCompiledTimeStamp = "2025-04-22 13:21:01.316667"
+export const lastCompiledTimeStamp = "2025-04-22 14:08:15.844034"
 
 export function UploadFilesProvider({ children }) {
   const [filesById, setFilesById] = useState({})
@@ -85,31 +85,31 @@ export function EventLoopProvider({ children }) {
 export function StateProvider({ children }) {
   const [reflex___state____state, dispatch_reflex___state____state] = useReducer(applyDelta, initialState["reflex___state____state"])
   const [reflex___state____state__reflex___state____update_vars_internal_state, dispatch_reflex___state____state__reflex___state____update_vars_internal_state] = useReducer(applyDelta, initialState["reflex___state____state.reflex___state____update_vars_internal_state"])
+  const [reflex___state____state__frontend___pages___search____search_state, dispatch_reflex___state____state__frontend___pages___search____search_state] = useReducer(applyDelta, initialState["reflex___state____state.frontend___pages___search____search_state"])
   const [reflex___state____state__reflex___state____on_load_internal_state, dispatch_reflex___state____state__reflex___state____on_load_internal_state] = useReducer(applyDelta, initialState["reflex___state____state.reflex___state____on_load_internal_state"])
   const [reflex___state____state__reflex___state____frontend_event_exception_state, dispatch_reflex___state____state__reflex___state____frontend_event_exception_state] = useReducer(applyDelta, initialState["reflex___state____state.reflex___state____frontend_event_exception_state"])
-  const [reflex___state____state__frontend___pages___search____search_state, dispatch_reflex___state____state__frontend___pages___search____search_state] = useReducer(applyDelta, initialState["reflex___state____state.frontend___pages___search____search_state"])
   const dispatchers = useMemo(() => {
     return {
       "reflex___state____state": dispatch_reflex___state____state,
       "reflex___state____state.reflex___state____update_vars_internal_state": dispatch_reflex___state____state__reflex___state____update_vars_internal_state,
+      "reflex___state____state.frontend___pages___search____search_state": dispatch_reflex___state____state__frontend___pages___search____search_state,
       "reflex___state____state.reflex___state____on_load_internal_state": dispatch_reflex___state____state__reflex___state____on_load_internal_state,
       "reflex___state____state.reflex___state____frontend_event_exception_state": dispatch_reflex___state____state__reflex___state____frontend_event_exception_state,
-      "reflex___state____state.frontend___pages___search____search_state": dispatch_reflex___state____state__frontend___pages___search____search_state,
     }
   }, [])
 
   return (
     <StateContexts.reflex___state____state value={ reflex___state____state }>
     <StateContexts.reflex___state____state__reflex___state____update_vars_internal_state value={ reflex___state____state__reflex___state____update_vars_internal_state }>
+    <StateContexts.reflex___state____state__frontend___pages___search____search_state value={ reflex___state____state__frontend___pages___search____search_state }>
     <StateContexts.reflex___state____state__reflex___state____on_load_internal_state value={ reflex___state____state__reflex___state____on_load_internal_state }>
     <StateContexts.reflex___state____state__reflex___state____frontend_event_exception_state value={ reflex___state____state__reflex___state____frontend_event_exception_state }>
-    <StateContexts.reflex___state____state__frontend___pages___search____search_state value={ reflex___state____state__frontend___pages___search____search_state }>
       <DispatchContext value={dispatchers}>
         {children}
       </DispatchContext>
-    </StateContexts.reflex___state____state__frontend___pages___search____search_state>
     </StateContexts.reflex___state____state__reflex___state____frontend_event_exception_state>
     </StateContexts.reflex___state____state__reflex___state____on_load_internal_state>
+    </StateContexts.reflex___state____state__frontend___pages___search____search_state>
     </StateContexts.reflex___state____state__reflex___state____update_vars_internal_state>
     </StateContexts.reflex___state____state>
   )
